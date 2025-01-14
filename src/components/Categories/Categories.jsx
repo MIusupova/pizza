1e06ca453e3e5ef62c8570a2007a2a102ee4e511
MIusupova <li.yusupova.01@mail.ru>
2025-01-15 00:22:15 +0100

    nn

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const categories = ['Все', 'Мясные', 'Вегетерианские', 'Гриль', 'Острые', 'Закрытые']
+    
+    return (
+        <div className="categories">
+        <ul>
+          {categories.map((value, i) => (
+            <li key={i} onClick={() => setActiveIndex(i)} className={activeIndex === i ? 'active' : ''}> {value}</li>
+          ))}
+        </ul>
+      </div>
+
+                
+    )
+}
+export default Categories;
